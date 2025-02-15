cef0bea09b004155518bdaed737205b297d87895
Ritesh Kumar <157762698+Ritesh1244@users.noreply.github.com>
2025-02-15 22:09:34 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Routes, Route, Navigate } from "react-router-dom";
 import './App.css';
 import Home from './pages/Home';
-import Signup from './pages/signup';
+import Signup from './pages/Signup';
 import Login from './pages/Login';
 import RefreshHandler from './RefreshHandler';
 
